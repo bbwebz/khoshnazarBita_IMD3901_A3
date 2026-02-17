0600df5ccf71478f38b324470e21327edc95f1fe
bbwebz <bitanazar5984@gmail.com>
2026-02-16 19:58:44 -0500

    semi working
    can detect when the client or host presses i to pick up an interactable object

---
+++ khoshnazarBita_IMD3901_A3/Assets/Scripts/Networking_Scripts/PlayerInteractionNet.cs
@@ -0,0 +1,66 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteractionNet : NetworkBehaviour
+{
+    public float interactRange = 5f;
+    public Camera playerCamera;
+
+    public Crosshair crosshair_access;
+    public PickupController pickupController_access;
+    //public TileAnimate tileAnimate_access;
+   
+    void Update()
+    {
+        //check to see if its the HOST
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        //creating a ray that shoots out of the camera to detect objects in front of us
+        //we want the ray to be in front of us
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit; //hit box for the ray cast
+
+        if (Physics.Raycast(ray, out hit, interactRange))
+        {
+            if (hit.collider.CompareTag("Interactable"))
+            {
+                //checking if the ray hits something with a collider that is interactable
+                crosshair_access.setInteract(true);
+
+                if (Keyboard.current.iKey.wasPressedThisFrame)
+                {
+                    if (IsHost)
+                        Debug.Log("HOST pressed I");
+
+                    else if (IsClient)
+                        Debug.Log("CLIENT pressed I");
+                }
+
+
+
+                /*
+                if (Keyboard.current.pKey.wasPressedThisFrame)
+                {
+                    Debug.Log("tile pressed was: " + hit.collider.gameObject.name);
+                    //tileAnimate_access.Press();
+
+                    //trigger the press function only for the tile that was hit with the ray
+                    TileAnimate tile = hit.collider.GetComponent<TileAnimate>();
+                    if (tile != null)
+                    {
+                        tile.Press();
+                    }
+                }
+                */
+
+                //Debug.Log("interact was set to true");
+                return;
+            }
+        }
+        crosshair_access.setInteract(false); //set it back to false if we look away from the object
+    }
+}
