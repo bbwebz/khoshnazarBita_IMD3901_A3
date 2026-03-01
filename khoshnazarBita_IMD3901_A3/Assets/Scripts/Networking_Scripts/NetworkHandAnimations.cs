279482b6eb5815a9cdd931915de1d6747172d57e
bbwebz <bitanazar5984@gmail.com>
2026-03-01 17:19:23 -0500

    synching vr hand animations
    hand animations for grip and trigger now update for host and client properly with RPC calls

--- khoshnazarBita_IMD3901_A3/Assets/Scripts/Networking_Scripts/NetworkHandAnimations.cs
+++ khoshnazarBita_IMD3901_A3/Assets/Scripts/Networking_Scripts/NetworkHandAnimations.cs
@@ -19,6 +19,25 @@ public class NetworkHandAnimations : NetworkBehaviour
             //change the trigger value in the animator
             handAnimator.SetFloat("Trigger", trigger);
             handAnimator.SetFloat("Grip", grip);
+
+            //update host, then within that call function to update client
+            SubmitHandAnimationServerRpc(trigger, grip);
         }
+    }
+
+    [ServerRpc] //run code on server to update the host's hands
+    void SubmitHandAnimationServerRpc(float trigger, float grip)
+    {
+        //sned the values to the client
+        UpdateHandAnimationClientRpc(trigger, grip);
+    }
+
+    [ClientRpc] //run code on client to update the client's hands
+    void UpdateHandAnimationClientRpc(float trigger, float grip)
+    {
+        if (IsOwner) return; //owner already updated locally
+
+        handAnimator.SetFloat("Trigger", trigger);
+        handAnimator.SetFloat("Grip", grip);
     }
 }
