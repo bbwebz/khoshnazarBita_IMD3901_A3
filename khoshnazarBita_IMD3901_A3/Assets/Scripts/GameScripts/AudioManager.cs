a48bdce95019bfbfd3792fcfcf851287f7ecf898
bbwebz <bitanazar5984@gmail.com>
2026-03-04 11:20:35 -0500

    added more sounds
    both players can play all unique sounds

--- khoshnazarBita_IMD3901_A3/Assets/Scripts/GameScripts/AudioManager.cs
+++ khoshnazarBita_IMD3901_A3/Assets/Scripts/GameScripts/AudioManager.cs
@@ -11,7 +11,14 @@ public class AudioManager : NetworkBehaviour
     //public AudioClip background;
     public AudioClip Tile1;
     public AudioClip Tile2;
-    //public AudioClip Tile3;
+    public AudioClip Tile3;
+    public AudioClip Tile4;
+    public AudioClip Tile5;
+    public AudioClip Tile6;
+    public AudioClip Tile7;
+    public AudioClip Tile8;
+    public AudioClip Tile9;
+
 
     //store strings of which tile each player pressed (for comparison later on)
     string player1Sound; 
@@ -49,16 +56,36 @@ public class AudioManager : NetworkBehaviour
             Debug.Log("client played " + tileName + " sound");
         }
 
+        //play sound according to which tile was pressed (each tile plays a unique note)
         switch (tileName)
         {
-
             case "Tile1":
                 PlaySFX(Tile1);
                 break;
             case "Tile2":
                 PlaySFX(Tile2);
                 break;
-
+            case "Tile3":
+                PlaySFX(Tile3);
+                break;
+            case "Tile4":
+                PlaySFX(Tile4);
+                break;
+            case "Tile5":
+                PlaySFX(Tile5);
+                break;
+            case "Tile6":
+                PlaySFX(Tile6);
+                break;
+            case "Tile7":
+                PlaySFX(Tile7);
+                break;
+            case "Tile8":
+                PlaySFX(Tile8);
+                break;
+            case "Tile9":
+                PlaySFX(Tile9);
+                break;
         }
     }
 
